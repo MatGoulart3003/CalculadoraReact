5ead290794b151ca0e34da7e9f51581d4ef9662c
MatGoulart3003 <mat_goulart3003@yahoo.com.br>
2023-03-23 13:12:02 -0300

    commit de correcao do inputNum

--- calculator/src/Components/Calculator.jsx
+++ calculator/src/Components/Calculator.jsx
@@ -1,32 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Calculator.css'
 
 export default function Calculator() {
- 
+
+  const [num, setNum] = useState(0)
+
+  function inputNum(value){
+    if(num === "c"){
+      setNum(num)
+    }else if (num===0){
+      setNum(value.target.value)
+    }else{
+      setNum(num+value.target.value)
+    }
+  }
+  
+  function clear(value) {
+    setNum(0)
+  }
    return (
     <div className='box'>
-      <h1 className='operation'>1-1</h1>
+      <h1 className='operation'>{num}</h1>
       <h1 className='result'>0</h1>
-        <button className='buttonRight'>C</button>
-        <button className='buttonLeft'>/</button>
+        <button className='buttonRight' onClick={clear} value={"c"}>C</button>
+        <button className='buttonLeft' onClick={inputNum} value={"/"}>/</button>
 
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>X</button>        
+        <button onClick={inputNum} value={1}>1</button>
+        <button onClick={inputNum} value={2}>2</button>
+        <button onClick={inputNum} value={3}>3</button>
+        <button onClick={inputNum} value={'*'}>X</button>        
         
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
+        <button onClick={inputNum} value={4}>4</button>
+        <button onClick={inputNum} value={5}>5</button>
+        <button onClick={inputNum} value={6}>6</button>
+        <button onClick={inputNum} value={"+"}>+</button>
 
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>-</button>
+        <button onClick={inputNum} value={7}>7</button>
+        <button onClick={inputNum} value={8}>8</button>
+        <button onClick={inputNum} value={9}>9</button>
+        <button onClick={inputNum} value={'-'}>-</button>
 
-        <button className='buttonRight'>0</button>      
-        <button className='buttonLeft'>=</button>
+        <button className='buttonRight' onClick={inputNum} value={0}>0</button>      
+        <button className='buttonLeft' onClick={inputNum} value={"="}>=</button>
     </div>
   )
 }
